bf5c852e95cb4e5e0a6ea8cde3ed1d0929c277ab
AroAceRulez <115325815+AroAceRulez@users.noreply.github.com>
2023-02-11 11:51:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 music1="";
 music2="";
 function preload(){
@@ -12,8 +16,23 @@ canvas.position(470,250);
 video = createCapture(VIDEO);
 video.hide();
 
+poseNet= ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 
+function gotPoses(results){
+   if(results.length > 0){
+    console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+   }
+}
+
+function modelLoaded(){
+    console.log('Model has been loaded');
+}
 function draw(){
 image(video,0,0,600,500)
 }
